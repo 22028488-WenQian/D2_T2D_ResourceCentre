b6e246d0f90496350f50ee6e9441d4f206f33981
22028488 <22028488@Sky>
2023-07-11 22:40:14 +0800

    updated - wq
    Merge branch 'master' of
    https://github.com/22028488-WenQian/D2_T2D_ResourceCentre.git

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -167,9 +167,9 @@ public class ResourceCentreTest {
 		assertFalse("Test that the loan fails.", ok);
 
 		// Test case 4 - Missing details
-		ResourceCentre.addCamcorder(camcorderList, cc3);
-		assertTrue("Test that there is an item available", camcorderList.get(2).getIsAvailable());
-		ok = ResourceCentre.doLoanCamcorder(camcorderList, "CB0013", "" );
+		ResourceCentre.addChromebook(chromebookList, cb3);
+		assertTrue("Test that there is an item available", chromebookList.get(2).getIsAvailable());
+		ok = ResourceCentre.doLoanChromebook(chromebookList, "CB0013", "" );
 		assertFalse("Test that the loan fails.", ok);
 	}
 
